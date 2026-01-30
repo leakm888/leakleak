da0205f1a1390aff0acc0dbf485d60f4bfcb6dc5
leakm888 <leakm928@gmail.com>
2026-01-30 15:06:52 +0700

    Add files via upload

---
+++ JS/da.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const btn = document.getElementById("myBtn");
+  btn.addEventListener("click", function() {
+    alert("Button clicked!");
+  });
+});
